abcd94f6fe2c07760610955047be9f0292446702
jsmolka <jsmolka@pm.me>
2021-12-07 19:32:20 +0100

    remove vector from main

--- include/sh/main.h
+++ include/sh/main.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <sh/env.h>
-#include <sh/vector.h>
+#include <memory>
+
 #include <sh/windows.h>
 
 #if SH_CC_MSVC
@@ -13,20 +13,18 @@ auto main(int argc, char* argv[]) -> int;
 // clang-format on
 
 auto wmain(int argc, wchar_t* argv[]) -> int {
-  sh::vector<char*> args;
-  for (int i = 0; i < argc; ++i) {
-    const auto size = WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, NULL, 0, NULL, NULL);
-    const auto data = new char[std::max(size + 1, 2)];
-    WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, data, size, NULL, NULL);
-    data[size + 1] = '\0';
-    args.push_back(data);
-  }
+  static_assert(sizeof(std::unique_ptr<char[]>) == sizeof(char*));
+  static_assert(sizeof(std::unique_ptr<std::unique_ptr<char[]>[]>) == sizeof(char**));
 
-  const auto value = main(argc, args.data());
-  for (const auto& data : args) {
-    delete[] data;
+  auto args = std::make_unique<std::unique_ptr<char[]>[]>(argc);
+  for (int i = 0; i < argc; ++i) {
+    auto size = WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, NULL, 0, NULL, NULL);
+    auto data = std::make_unique<char[]>(std::max(size + 1, 2));
+    WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, data.get(), size, NULL, NULL);
+    data[size + 1] = '\0';
+    args[i] = std::move(data);
   }
-  return value;
+  return main(argc, reinterpret_cast<char**>(args.get()));
 }
 
 #endif
