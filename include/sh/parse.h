9314d37b5a3d5ad909e535b2e9ddb1fe23244d83
jsmolka <jsmolka@pm.me>
2021-12-08 19:19:58 +0100

    remove polyfill

--- include/sh/parse.h
+++ include/sh/parse.h
@@ -7,7 +7,13 @@
 #include <string_view>
 
 #include <sh/concepts.h>
-#include <sh/polyfill.h>
+#include <sh/fast_float/fast_float.h>
+
+namespace std {
+
+using fast_float::from_chars;
+
+}  // namespace std
 
 namespace sh {
 
--- include/sh/polyfill.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sh/fast_float/fast_float.h>
-
-namespace std {
-
-using fast_float::from_chars;
-
-}  // namespace std
