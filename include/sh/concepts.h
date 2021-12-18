4cdeb584cfbc9736fc873f433c09a97b79de2b6c
jsmolka <jsmolka@pm.me>
2021-12-18 21:13:45 +0100

    add more traits

--- include/sh/concepts.h
+++ include/sh/concepts.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <concepts>
-#include <type_traits>
+
+#include <sh/traits.h>
 
 namespace sh {
 
@@ -21,7 +22,7 @@ template <typename T, typename U>
 concept different_from = !std::same_as<T, U>;
 
 template <typename T, typename... Ts>
-concept any_of = std::disjunction_v<std::is_same<T, Ts>...>;
+concept any_of = is_any_of_v<T, Ts...>;
 
 template <typename... Ts>
 concept not_empty = sizeof...(Ts) > 0;
--- include/sh/traits.h
+++ include/sh/traits.h
@@ -4,6 +4,12 @@
 
 namespace sh {
 
+template <typename T, typename... Ts>
+inline constexpr bool is_any_of_v = std::disjunction_v<std::is_same<T, Ts>...>;
+
+template <typename T, typename... Ts>
+struct is_any_of : std::bool_constant<is_any_of_v<T, Ts...>> {};
+
 template <typename T, template <typename...> typename Template>
 inline constexpr bool is_specialization_v = false;
 
@@ -12,5 +18,13 @@ inline constexpr bool is_specialization_v<Template<Ts...>, Template> = true;
 
 template <typename T, template <typename...> typename Template>
 struct is_specialization : std::bool_constant<is_specialization_v<T, Template>> {};
+
+template <typename T>
+struct unqualified {
+  using type = std::remove_cv_t<std::remove_pointer_t<std::decay_t<T>>>;
+};
+
+template <typename T>
+using unqualified_t = typename unqualified<T>::type;
 
 }  // namespace sh
