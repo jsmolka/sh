8cadd0cdfb6576fc46ff6fa2c7f22cb01cb1b1b5
jsmolka <jsmolka@pm.me>
2022-01-01 21:48:39 +0100

    improve clap

--- include/sh/clap.h
+++ include/sh/clap.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <memory>
-#include <string_view>
 #include <vector>
 
 #include <sh/concepts.h>
@@ -18,29 +17,29 @@ template<formattable T>
 auto repr(const T& value) -> std::string {
   if constexpr (std::convertible_to<T, std::string_view>) {
     return fmt::format(R"("{}")", std::string_view{value});
-  } else if constexpr (std::same_as<T, filesystem::path>) {
+  } else if constexpr (std::same_as<T, std::filesystem::path>) {
     return fmt::format(R"("{}")", value);
   } else {
     return fmt::to_string(value);
   }
 }
 
-inline auto split(std::string_view str, char delimiter) -> std::vector<std::string_view> {
-  const auto pos = str.find_first_of(delimiter);
+inline auto split(std::string_view view, char delimiter) -> std::vector<std::string_view> {
+  const auto pos = view.find_first_of(delimiter);
   if (pos == std::string_view::npos) {
-    return {str};
+    return {view};
   } else {
-    return {str.substr(0, pos), str.substr(pos + 1)};
+    return {view.substr(0, pos), view.substr(pos + 1)};
   }
 }
 
-inline auto trim(std::string_view str) -> std::string_view {
-  const auto first = str.find_first_not_of(' ');
-  if (std::string_view::npos == first) {
-    return str;
+inline auto trim(std::string_view view) -> std::string_view {
+  const auto pos = view.find_first_not_of(' ');
+  if (std::string_view::npos == pos) {
+    return view;
   }
-  const auto last = str.find_last_not_of(' ');
-  return str.substr(first, last - first + 1);
+  const auto end = view.find_last_not_of(' ');
+  return view.substr(pos, end - pos + 1);
 }
 
 template<typename T>
@@ -71,8 +70,13 @@ public:
     : names_(names) {}
 
 protected:
+  virtual auto boolean() const -> bool = 0;
+  virtual auto optional() const -> bool = 0;
+  virtual void validate() const = 0;
+  virtual void parse(std::optional<std::string_view>) = 0;
+
   std::vector<std::string_view> names_;
-  std::string_view name_ = "value";
+  std::string_view what_ = "value";
   std::string_view desc_;
   std::string default_repr_;
 
@@ -95,7 +99,7 @@ private:
     if (positional()) {
       usage = fmt::format("<{}>", usage);
     } else if (!boolean()) {
-      usage = fmt::format("{} <{}>", usage, name_);
+      usage = fmt::format("{} <{}>", usage, what_);
     }
     if (optional()) {
       usage = fmt::format("[{}]", usage);
@@ -104,23 +108,18 @@ private:
   }
 
   auto description() const -> std::string {
-    std::vector<std::string> parts;
+    std::vector<std::string> desc;
     if (!desc_.empty()) {
-      parts.emplace_back(desc_);
+      desc.emplace_back(desc_);
     }
     if (!default_repr_.empty()) {
-      parts.emplace_back(fmt::format("(default: {})", default_repr_));
+      desc.emplace_back(fmt::format("[default: {}]", default_repr_));
     }
-    return fmt::to_string(fmt::join(parts, " "));
+    return fmt::to_string(fmt::join(desc, " "));
   }
-
-  virtual auto boolean() const -> bool = 0;
-  virtual auto optional() const -> bool = 0;
-  virtual void validate() const = 0;
-  virtual void parse(std::optional<std::string_view>) = 0;
 };
 
-class name : public std::string_view {};
+class what : public std::string_view {};
 class desc : public std::string_view {};
 
 template<argument_type T>
@@ -129,6 +128,7 @@ public:
   friend class clap;
 
   using value_type = value_type_t<T>;
+  using event = std::function<void(const value_type&)>;
 
   explicit argument(const std::vector<std::string_view>& names)
     : basic_argument(names) {
@@ -146,12 +146,12 @@ public:
     return *this;
   }
 
-  auto operator<<(sh::name name) -> argument& {
-    name_ = name;
+  auto operator<<(what what) -> argument& {
+    what_ = what;
     return *this;
   }
 
-  auto operator<<(sh::desc desc) -> argument& {
+  auto operator<<(desc desc) -> argument& {
     desc_ = desc;
     return *this;
   }
@@ -163,12 +163,12 @@ public:
     return *this;
   }
 
-  auto operator<<(const std::function<void(const value_type&)>& event) -> argument& {
+  auto operator<<(const event& event) -> argument& {
     events_.emplace_back(event);
     return *this;
   }
 
-private:
+protected:
   auto boolean() const -> bool final {
     return std::same_as<value_type, bool>;
   }
@@ -206,6 +206,7 @@ private:
     }
   }
 
+private:
   T value() const {
     if (value_) {
       return *value_;
@@ -226,7 +227,7 @@ private:
   T* pointer_ = nullptr;
   std::optional<T> value_;
   std::optional<T> default_;
-  std::vector<std::function<void(const value_type&)>> events_;
+  std::vector<event> events_;
 };
 
 class clap {
@@ -238,7 +239,7 @@ public:
     requires not_empty<Names...>
   auto add(Names&&... names) -> argument<T>& {
     const auto views = {trim(names)...};
-    arguments_.push_back(std::make_unique<argument<T>>(views));
+    arguments_.emplace_back(std::make_unique<argument<T>>(views));
     return *static_cast<argument<T>*>(arguments_.back().get());
   }
 
@@ -253,18 +254,18 @@ public:
 
   void parse(int argc, const char* const* argv) {
     auto index = 1;
-    auto positional_index = 0;
-    auto positional_force = false;
+    auto pos_index = 0;
+    auto pos_force = false;
     while (index < argc) {
       const auto& data = trim(argv[index++]);
-      if (data == "--" && !positional_force) {
-        positional_force = true;
+      if (data == "--" && !pos_force) {
+        pos_force = true;
         continue;
       }
 
       const auto kvp = split(data, '=');
       const auto argument = find(kvp.front());
-      if (argument && !argument->positional() && !positional_force) {
+      if (argument && !argument->positional() && !pos_force) {
         if (kvp.size() == 2) {
           argument->parse(kvp.back());
         } else if (index < argc && !argument->boolean()) {
@@ -273,7 +274,7 @@ public:
           argument->parse(std::nullopt);
         }
       } else {
-        if (const auto argument = find(positional_index++)) {
+        if (const auto argument = find(pos_index++)) {
           argument->parse(data);
         } else {
           throw error("unmatched positional argument: {}", data);
@@ -319,7 +320,7 @@ public:
       std::string content;
     };
 
-    std::array<group, 2> groups = { "keyword arguments", "positional arguments" };
+    group groups[2] = {{"keyword arguments"}, {"positional arguments"}};
     for (const auto& argument : arguments_) {
       auto& group = groups[argument->positional()];
       group.content.append(fmt::format("\n  {:<{}}{}", argument->names(), padding + 4, argument->description()));
@@ -327,7 +328,7 @@ public:
     }
 
     auto help = fmt::format("usage:\n  {}", fmt::join(usage, " "));
-    for (const auto& [caption, content] : reversed(groups)) {
+    for (const auto& [caption, content] : groups) {
       if (!content.empty()) {
         help.append(fmt::format("\n\n{}:", caption));
         help.append(content);
